b4a17b6b16064410c387b405946158169aaba31a
Mahfouz-z <mohamed.mahfouz@aucegypt.edu>
2024-02-27 02:33:25 +0100

    Updating naming

--- post-script.sh
+++ post-script.sh
@@ -1,11 +1,11 @@
 # Install helper libraries
 mkdir ~/repos/HardCilk
 cp -r /mnt/* ~/repos/HardCilk  
-cd ~/repos/HardCilk/chisel3-interface/chisel3-interface
-sbt publishLocal
 cd ~/repos/HardCilk/strenc-scala
+sbt publishLocal
+cd ~/repos/HardCilk/chisel3-interface/chisel3-interface
 sbt publishLocal
 cd
 python3 -m pip install -e ~/repos/HardCilk/strenc-python
 python3 -m pip install -e ~/repos/HardCilk/chisel3-interface/clients/python
-
+python3 -m pip install -e ~/repos/HardCilk/systemc_helpers/python
