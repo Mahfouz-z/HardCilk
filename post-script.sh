d8302064c2841ba4d34cb255d79c89e238580a45
Mahfouz-z <mohamed.mahfouz@aucegypt.edu>
2024-03-06 01:16:27 +0100

    update eval

--- post-script.sh
+++ post-script.sh
@@ -1,20 +1,31 @@
 # Install helper libraries
-# mkdir ~/repos/HardCilk
-# cp -r /mnt/* ~/repos/HardCilk  
-# cd ~/repos/HardCilk/strenc-scala
-# sbt publishLocal
-# cd ~/repos/HardCilk/chisel3-interface/chisel3-interface
-# sbt publishLocal
-# cd
-# python3 -m pip install -e ~/repos/HardCilk/strenc-python
-# python3 -m pip install -e ~/repos/HardCilk/chisel3-interface/clients/python
-# python3 -m pip install -e ~/repos/HardCilk/systemc_helpers/python
-
-cd ~/strenc-scala
+mkdir ~/repos/HardCilk
+cp -r /mnt/* ~/repos/HardCilk  
+cd ~/repos/HardCilk/strenc-scala
 sbt publishLocal
-cd ~/chisel3-interface/chisel3-interface
+cd ~/repos/HardCilk/chisel3-interface/chisel3-interface
 sbt publishLocal
 cd
-python3 -m pip install -e ~/strenc-python
-python3 -m pip install -e ~/chisel3-interface/clients/python
-python3 -m pip install -e ~/systemc_helpers/python
+python3 -m pip install -e ~/repos/HardCilk/strenc-python
+python3 -m pip install -e ~/repos/HardCilk/chisel3-interface/clients/python
+python3 -m pip install -e ~/repos/HardCilk/systemc_helpers/python
+
+# cd ~/strenc-scala
+# sbt publishLocal
+# cd ~/chisel3-interface/chisel3-interface
+# sbt publishLocal
+# cd
+# python3 -m pip install -e ~/strenc-python
+# python3 -m pip install -e ~/chisel3-interface/clients/python
+# python3 -m pip install -e ~/systemc_helpers/python
+
+# mkdir ~/HardCilk
+# cp -r /code/* ~/HardCilk  
+# cd ~/HardCilk/strenc-scala
+# sbt publishLocal
+# cd ~/HardCilk/chisel3-interface/chisel3-interface
+# sbt publishLocal
+# cd
+# python3 -m pip install -e ~/HardCilk/strenc-python
+# python3 -m pip install -e ~/HardCilk/chisel3-interface/clients/python
+# python3 -m pip install -e ~/HardCilk/systemc_helpers/python
--- run_evaluation.py
+++ run_evaluation.py
@@ -1,41 +1,73 @@
 # Run the post script to install necessary packages and run the experiment
 import subprocess
-import os
 import time
+import threading
 
-subprocess.Popen('sh post-script.sh', shell=True)
+
+p = subprocess.Popen('sh post-script.sh', shell=True)
+p.wait()
+
 
 def run_arch_gen():
     subprocess.Popen('cd architecture-generator/ && exec sbt "runMain fullSysGen.ServerEmitter"', shell=True)
 
-processid = os.fork()
 
-if processid > 0 :
-    run_arch_gen()
-
-else:
-    time.sleep(30) # Wait some time for the archGen server to launch
+def runTesting():
+    time.sleep(30)  
     
     # Generate the circuits to be simulated
-    subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp_dae_0 && python3 generate_bulk.py', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp_dae_1 && python3 generate_bulk.py', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp1 && python3 generate_bulk.py', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp2 && python3 generate_bulk.py', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp3 && python3 generate_bulk.py', shell=True)
+    g1 = subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp_dae_0 && python3 generate_bulk.py', shell=True)
+    g2 = subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp_dae_1 && python3 generate_bulk.py', shell=True)
+    g3 = subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp1 && python3 generate_bulk.py', shell=True)
+    g4 = subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp2 && python3 generate_bulk.py', shell=True)
+    g5 = subprocess.Popen('cd evaluation/HardCilk/simulations/paper_exp3 && python3 generate_bulk.py', shell=True)
+
+    g1.wait()
+    g2.wait()
+    g3.wait()
+    g4.wait()
+    g5.wait()
 
     # Run cmake and make
-    subprocess.Popen('cd evaluation/HardCilk && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j 8', shell=True)
-
+    p = subprocess.Popen('cd evaluation/HardCilk && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j 8', shell=True)
+    p.wait()
+    
     # symbolic links for the runner
-    subprocess.Popen('cd evaluation/HardCilk/simulations && mkdir run && mkdir run/bin', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp_dae_0/simulation_paper_exp_dae_0', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp_dae_1/simulation_paper_exp_dae_1', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp1/simulation_paper_exp1', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp2/simulation_paper_exp2', shell=True)
-    subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp3/simulation_paper_exp3', shell=True)
+    p = subprocess.Popen('cd evaluation/HardCilk/simulations && mkdir run && mkdir run/bin', shell=True)
+    p.wait()    
+    
+    l1 = subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp_dae_0/simulation_paper_exp_dae_0', shell=True)
+    l2 = subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp_dae_1/simulation_paper_exp_dae_1', shell=True)
+    l3 = subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp1/simulation_paper_exp1', shell=True)
+    l4 = subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp2/simulation_paper_exp2', shell=True)
+    l5 = subprocess.Popen('cd evaluation/HardCilk/simulations/run/bin && ln -s ../../../build/simulations/paper_exp3/simulation_paper_exp3', shell=True)
+
+    l1.wait()
+    l2.wait()
+    l3.wait()
+    l4.wait()
+    l5.wait()
+
 
     # Run the experiments
-    subprocess.Popen('cd evaluation/HardCilk/simulations && python3 run_exp_all.py', shell=True)
+    p = subprocess.Popen('cd evaluation/HardCilk/simulations && python3 run_exp_all.py --run --force --num-workers 8', shell=True)
+    p.wait()
 
     # copy results to /results
-    subprocess.Popen('cp -r evaluation/HardCilk/simulations/run /results', shell=True)
+    p = subprocess.Popen('cp -r evaluation/HardCilk/simulations/run /results', shell=True)
+    p.wait()
+
+    print("Experiments Finished!")
+
+
+# Create threads for each function
+thread1 = threading.Thread(target=run_arch_gen)
+thread2 = threading.Thread(target=runTesting)
+
+# Start the threads
+thread1.start()
+thread2.start()
+
+# Join the the testing thread
+thread2.join()
+
